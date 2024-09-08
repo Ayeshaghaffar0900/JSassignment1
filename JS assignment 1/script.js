1b8db3422aac7f4e8d2cde802cd8121e0b891661
Ayeshaghaffar0900 <ayesha.a.ghaffar07@gmail.com>
2024-09-09 00:07:09 +0500

    Add files via upload

---
+++ JS assignment 1/script.js
@@ -0,0 +1,101 @@
+// alert("Welcome");
+
+
+// // var add = 3 + 5 , sub = 3 - 5 , mult = 3 * 5, div = 3 / 5 ;
+
+// // document.write("Sum of 3 and 5 is " + add  );
+// // document.write( "  sub of 3 and 5 is " + sub);
+// // document.write("   mult of 3 and 5 is " + mult);
+// // document.write("   div of 3 and 5 is " + div);
+
+//                                              // Chapter No 1
+
+// // Question No 1
+
+// // I
+
+// alert("Ayesha");
+
+// // II
+
+// alert("Abdul Ghaffar");
+
+
+// // III
+
+// alert(90078601);
+
+
+// // IV
+
+// alert("My email is example123@example.com");
+
+// // V
+
+// alert(78690);
+
+
+// // Question No 2
+// alert("You're learning JavaScript!");
+
+// // Question No 3
+// alert("Welcome to JS assignment 1!");
+
+
+//                                              // Chapter No 2
+
+// // Question No 1
+
+// var firstName = "Ayesha";                                             
+
+// // Question No 2
+
+// var firstName = "";
+
+// firstName = "ayesha";
+
+// alert(firstName);
+
+// // Question No 3
+
+
+// var teamName = "myteam";
+// alert(teamName);
+
+// // Question No 4
+// var bestMan = "Charlie"
+
+// bestMan = "John"
+
+// alert(bestMan);
+
+//                                              // Chapter No 3
+// // Question No 1&2
+// var caseQty = 144;
+// console.log(caseQty);
+
+// // Question No 4
+// var num = 5;
+// num = 5*3;
+
+// console.log(num);
+
+
+// // Question No 5
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var total = merchTotal + shippingCharge;
+
+// console.log(total);
+
+
+// // Question No 6
+
+// var num = 14;
+// num = ++num;
+
+// console.log(num);
+
+
+//                                                                               // End
